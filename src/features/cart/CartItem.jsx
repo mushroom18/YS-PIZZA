fb19437802c66e25e2b6ba1452cf5050b6d1d406
yushu <zhuysh18@163.com>
2025-03-12 19:01:33 +1100

    styling cart page

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,15 +1,17 @@
+import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
 
   return (
-    <li>
-      <p>
+    <li className="py-3 md:flex md:items-center md:justify-between">
+      <p className="mb-1 md:mb-0">
         {quantity}&times; {name}
       </p>
-      <div>
-        <p>{formatCurrency(totalPrice)}</p>
+      <div className="flex items-center justify-between md:gap-6">
+        <p className="text-small font-bold">{formatCurrency(totalPrice)}</p>
+        <Button type="small">Delete</Button>
       </div>
     </li>
   );
